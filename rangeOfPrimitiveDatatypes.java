b192c4879e76a65e9e225fa79c7c659e6e6e99a5
Vishnu Choudhary <31178173+vc7184@users.noreply.github.com>
2018-07-21 21:17:34 +0530

    Add files via upload

---
+++ rangeOfPrimitiveDatatypes.java
@@ -0,0 +1,13 @@
+import static java.lang.Byte.*;
+//import static java.lang.Short.*;
+//import static java.lang.Integer.*;
+//import static java.lang.Long.*;
+//import static java.lang.Float.*;
+//import static java.lang.Double.*;
+
+public class rangeOfPrimitiveDatatypes {
+    public static void main(String[] args) {
+        System.out.println(MIN_VALUE);
+        System.out.println(MAX_VALUE);
+    }
+}
