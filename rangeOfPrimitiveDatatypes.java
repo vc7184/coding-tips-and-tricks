761e2181b39fdf1b1fbb56c839c9e68817cd72e1
Vishnu Choudhary <31178173+vc7184@users.noreply.github.com>
2018-09-20 15:17:05 +0530

    Update rangeOfPrimitiveDatatypes.java

--- rangeOfPrimitiveDatatypes.java
+++ rangeOfPrimitiveDatatypes.java
@@ -10,4 +10,11 @@ public class rangeOfPrimitiveDatatypes {
         System.out.println(MIN_VALUE);
         System.out.println(MAX_VALUE);
     }
+    //ELSE YOU CAN USE THIS BUT DON'T USE ANY IMPORT STATEMENT
+    /*long x=sc.nextLong();
+    System.out.println(x+" can be fitted in:");
+    if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)System.out.println("* byte");
+    if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)System.out.println("* short");
+    if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)System.out.println("* int");
+    if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)System.out.println("* long");*/
 }
