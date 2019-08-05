0d07b8435d6c6a8218bb0fea63247d7f6a1aaecd
Vishnu Choudhary <31178173+vc7184@users.noreply.github.com>
2019-08-05 22:47:25 +0530

    Create GCD-of-two-num.cpp

---
+++ GCD-of-two-num.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ 
+int main()
+{
+    ll n,a;
+    cin>>n>>a;
+    cout<<(__gcd(n,a))<<endl;
+    return 0;
+}
